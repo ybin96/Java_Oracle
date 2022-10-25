2a00b19d2812af3cb71c34878ef2d0112ef57529
김영빈 ( Kim youngbin) <114568768+ybin96@users.noreply.github.com>
2022-10-25 09:14:57 +0900

    Add files via upload

---
+++ exam05/Test.java
@@ -0,0 +1,29 @@
+package exam01;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		EmpDAO dao = new EmpDAO();
+		ArrayList<EmpVO> list = dao.searchEmp("개발1팀");
+		for(EmpVO e : list) {
+			String ename = e.getEname();
+			int salary = e.getSalary();
+			String addr = e.getAddr();
+			Date hiredate = e.getHiredate();
+			
+			String row = ename+","+salary+","+addr+","+hiredate;
+			System.out.println(row);
+		}
+		
+		System.out.println("==================================");
+		
+		ArrayList<String> list2 =dao.listDname();
+		for(String dname : list2) {
+			System.out.println(dname);
+		}
+	}
+
+}
